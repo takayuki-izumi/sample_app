1cff09f263de3a2392f18a1805a110ef57581c0b
takayuki-izumi <jumhnv528@gmail.com>
2015-12-20 09:07:19 +0900

    Improve the README

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+ 
+ #ページごとに完全なタイトルを返す
+  def full_title(page_title)
+  	bace_titile = "Ruby on Rails Tutorial Sample App"
+  	if page_title.empty?
+  		bace_titile
+  	else
+  		"#{base_title} | #{page_title}"
+  	end
+  end
 end
